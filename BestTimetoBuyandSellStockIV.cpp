657f44b55fb1b8713f8698f91d141c3d2f1a613a
Satyam Borade <68340284+satyamsb1@users.noreply.github.com>
2022-09-10 11:56:38 +0530

    Update BestTimetoBuyandSellStockIV.cpp

--- BestTimetoBuyandSellStockIV.cpp
+++ BestTimetoBuyandSellStockIV.cpp
@@ -1,3 +1,4 @@
+//brute Force O(k*n^2)
 class Solution {
 public:
     int maxProfit(int k, vector<int>& p) {
@@ -17,6 +18,35 @@ public:
                     var = max(var, -p[prevj]+dp[i-1][prevj]);
                 }
                 dp[i][j] = max(p[j]+var, dp[i][j-1]);
+            }
+        }
+        return dp[k][n-1];
+    }
+};
+
+
+//Optimised Solution O(k*n)
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& p) {
+        int n =p.size();
+        if(n<=1) return 0;
+        
+        if(k>n)k=n;
+        
+        vector<vector<int>> dp(k+1, vector<int>(n, 0));
+        for(int i=1; i<k+1; i++)
+        {
+            int var = -p[0]+dp[i-1][0];
+            for(int j=1; j<n; j++)
+            {
+//                 for(int prevj = 1; prevj<j; prevj++)
+//                 {
+                   
+//                 }
+                dp[i][j] = max(p[j]+var, dp[i][j-1]);
+                 var = max(var, -p[j]+dp[i-1][j]);
             }
         }
         return dp[k][n-1];
